82a339b14f52163f2f5297154c0e2a6ed092294a
Saurish Srivastava <saurishs1215@gmail.com>
2020-12-26 14:40:12 -0800

    [update] fix tsx

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/global.css'
 import { ThemeProvider } from "../components/ThemeContext";
+import * as React from 'react'
+
 
 export default function App({ Component, pageProps }) {
   return (
